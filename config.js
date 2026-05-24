23cce4025e4cae04af0eb03a566644758efc57c1
fabiobentivegna-cell <fabio.bentivegna@gmail.com>
2026-05-24 14:21:13 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+// Configurazione Chiavi API personali - NON CARICARE SU GITHUB
+const API_CONFIG = {
+    FINNHUB_KEY: 9CKZO2SLSQZ47S8W,
+    ALPHA_VANTAGE_KEY: d850mk1r01qrqbnnsksgd850mk1r01qrqbnnskt0,
+    FRED_KEY: bf9ce29fa0c0dd9e6ccf82878e2f1519
+};
